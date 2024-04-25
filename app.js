3d11b8697b719eaf5d01298c0d6c4551f14d917f
Brendan Malycky <bmalycky@hotmail.com>
2024-04-25 12:53:48 -0600

    set up basic server code

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-// hi 
+import express from "express";
+const app = express(); // create a const using express object that was imported)
+const port = 3000;
+
+// 3000 is the port, then a callback function //
+app.listen(port, () => {
+  console.log(`server is running on port ${port}`);
+});
+
