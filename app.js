9115898452d30d06e5843c203441d27e555b9d28
Brendan Malycky <bmalycky@hotmail.com>
2024-04-26 11:59:01 -0600

    added routes

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
-import express from "express";
+import express from "express"; // remember to add type module to package.json file 
 const app = express(); // create a const using express object that was imported)
 const port = 3000;
+
+app.get("/", (req, res) => {
+  //console.log(req.rawHeaders);
+  res.send("<h1>Hello0?</h1>");
+})
+
+app.get("/about", (req, res) => {
+  //console.log(req.rawHeaders);
+  res.send("<h1>About Page</h1>");
+})
+
+app.get("/contact", (req, res) => {
+  //console.log(req.rawHeaders);
+  res.send("<h1>Contact Page</h1>");
+})
 
 // 3000 is the port, then a callback function //
 app.listen(port, () => {
